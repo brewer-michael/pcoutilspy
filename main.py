d0cadcadb9b3eac5f72554808bf638b70e911e62
brewer-michael <mb1985@gmail.com>
2022-06-06 14:13:50 -0400

    Support basic auth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #imports
+from msilib.schema import AppId
 import requests
+from requests.auth import HTTPBasicAuth
 import os
 from decouple import config
 from datetime import date
@@ -13,20 +15,20 @@ def main():
     url = 'https://api.planningcenteronline.com/publishing/v2/channels/3708/episodes'
     today = date.today()
     serviceDate = today.strftime('%B %d, %Y')
-    payload= {'application_id':APP_ID,'secret':SECRET, 'data':{'attributes':{'title':'Sunday, ' + serviceDate}}}
+    payload= {'data':{'attributes':{'title':'Sunday, ' + serviceDate}}}
     headers = {}
-    res = requests.post(url,data=payload)
+    res = requests.post(url,auth=HTTPBasicAuth(APP_ID,SECRET),data=payload)
     print(res)
     episodeId = res.id
     startsAt = today.strftime('%d/%m/%Y')
     startsAt = startsAt + 'T13:45:00+00:00'
-    youtubeEmbed = {'application_id':APP_ID,'secret':SECRET,'data':{'attributes':{'starts_at':startsAt,'video_embed_code':'<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/live_stream?autoplay=1&amp;channel=UCryZmERAkR6-fktliKiCGNA&amp;playsinline=1\' frameborder=\'0\' allow=\'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\' allowfullscreen></iframe>'}}}
+    youtubeEmbed = {'data':{'attributes':{'starts_at':startsAt,'video_embed_code':'<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/live_stream?autoplay=1&amp;channel=UCryZmERAkR6-fktliKiCGNA&amp;playsinline=1\' frameborder=\'0\' allow=\'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\' allowfullscreen></iframe>'}}}
     youtubeUrl = 'https://api.planningcenteronline.com/publishing/v2/episodes/' + episodeId + '/episode_times'
-    getepres = requests.get(youtubeUrl,data=youtubeEmbed)
+    getepres = requests.get(youtubeUrl,auth=HTTPBasicAuth(APP_ID,SECRET),data=youtubeEmbed)
     getepres()
     libraryUrl = 'https://api.planningcenteronline.com/publishing/v2/episodes/'+ episodeId +'/'
-    libraryData = {'application_id':APP_ID,'secret':SECRET,'data':{'attributes':{'published_to_library_at':{{startsAt}}}}}
-    addLibary = requests.patch(libraryUrl,libraryData)
+    libraryData = {'data':{'attributes':{'published_to_library_at':{{startsAt}}}}}
+    addLibary = requests.patch(libraryUrl,auth=HTTPBasicAuth(APP_ID,SECRET),data=libraryData)
     addLibary()
 
 if __name__ == "__main__":
