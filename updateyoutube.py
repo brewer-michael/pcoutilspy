518e48a6efacc982e05ee41caca49d3682e68f17
Michael Brewer <40496339+brewer-michael@users.noreply.github.com>
2023-12-12 21:13:22 -0500

    troubleshooting

--- updateyoutube.py
+++ updateyoutube.py
@@ -14,24 +14,27 @@ auth = {'application_id':APP_ID,'secret':SECRET}
 
 def main():
     #get most recent PCO sermon and update it once a live video is found at the specified youtube channel
-    apitoken = os.environ.get('YTKEY')	
+    apitoken = str(os.environ.get('YTKEY'))	
     today = date.today()
     serviceDate = today.strftime('%B %d, %Y\"')
     serviceDate = '\"Sunday, ' + serviceDate
-    pcoURL = 'https://api.planningcenteronline.com/publishing/v2/channels/3708/episodes?order=-published_live_at&page=1&where[search]=' + serviceDate
+    searchDate = date.today()
+    searchDate = searchDate.strftime('%B %d, %Y')
+    pcoURL = 'https://api.planningcenteronline.com/publishing/v2/channels/3708/episodes?order=-published_live_at&page=1&where[search]=' + searchDate
     res = requests.get(pcoURL,auth=HTTPBasicAuth(APP_ID,SECRET)).json()
+    print(res)	
     episodeId = res['data'][0]['id']
     #need to get back listing from youtube to update embed url accordingly
     #query episode id for starttimeid and assign youtube url
     startsAt = today.strftime('\"%Y-%m-%d')
     startsAt = startsAt + 'T13:45:00Z\"'
     youtubeUrl = 'https://api.planningcenteronline.com/publishing/v2/episodes/' + episodeId + '/episode_times'
-    getepres = requests.get(youtubeUrl,auth=HTTPBasicAuth(APP_ID,SECRET),data=youtubeEmbed).json()
+    getepres = requests.get(youtubeUrl,auth=HTTPBasicAuth(APP_ID,SECRET)).json()
     #print(getepres)
     episodeTimeId = getepres['data'][0]['id']
     print(episodeTimeId)
     episodeTimeURL = 'https://api.planningcenteronline.com/publishing/v2/episodes/'+ episodeId + '/episode_times/'+ episodeTimeId
-    episodeTimeId = getepres['items'][0]['id']
+    episodeTimeId = getepres['data'][0]['id']
     print(episodeTimeId)
     #create a wait timer to get a valid youtube video id or else fail out the file
     def GetYoutubeVideoId(apitoken):
@@ -60,7 +63,7 @@ def main():
         print(patchIframe)
         #print(addLibrary)
     except Exception:
-           sys.exit()
+           exit()
 
 if __name__ == "__main__":
         try:
